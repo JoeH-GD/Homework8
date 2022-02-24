7a4355169568ac5db58ecd9dc97c9a08185f3a82
JoeH-GD <joe.haldon.gd@gmail.com>
2022-02-24 10:59:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type dateTime = typeof(DateTime);
+            PropertyInfo [] properties = dateTime.GetProperties();
+
+            
+            for (int i=0; i<properties.Length; i ++)
+            {
+                Console.WriteLine(properties[i].ToString());
+            }
+            
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
